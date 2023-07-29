5d3c506fe747898a17f3a1496187274f06d60b78
danhigg <higgins.daniel@gmail.com>
2023-07-29 17:23:11 +1000

    define prompts, output to README

--- index.js
+++ index.js
@@ -1,13 +1,93 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const generateREADME = ({projectTitle, projectDesc, installation, usage, contribution, testInstr, license, github, email}) =>
+  `# ${projectTitle}
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+  ## Description
+  ${projectDesc}
 
-// TODO: Create a function to initialize app
-function init() {}
+  ## Table of Contents
+  
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  
+  ## Installation
+  ${installation}
 
-// Function call to initialize app
-init();
+  ## Usage
+  ${usage}
+
+  ## License
+  This applicattion is covered under ${license}
+
+  ## Contributing
+  ${contribution}
+
+  ## Tests
+  ${testInstr}
+
+  ## Questions
+  Github: https://github.com/${github}  
+  Email: ${email}
+  `;
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'projectTitle',
+      message: 'What is the title of your project?',
+    },
+    {
+      type: 'input',
+      name: 'projectDesc',
+      message: 'Briefly describe your project.',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'What are the installation requirements?',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'What is the usage information?',
+    },
+    {
+      type: 'input',
+      name: 'contribution',
+      message: 'What are the contribution guidelines?',
+    },
+    {
+      type: 'input',
+      name: 'testInstr',
+      message: 'What are the test instructions?',
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'What license covers this application?',
+      choices: ['license1', 'license2', 'license3', 'license4']
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'What is your github username?',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'What is your email address?',
+    },
+  ])
+  .then((answers) => {
+    console.log(answers);
+    fs.writeFile('./output/README.md', generateREADME(answers), (err) =>
+      err ? console.log(err) : console.log('Created README.md')
+    );
+  });
