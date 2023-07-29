f20f14681c229a9c34ec00333a090754c0835cd4
danhigg <higgins.daniel@gmail.com>
2023-07-29 19:48:00 +1000

    inset license badge

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-README-generator
+# README Generator
+
+![License: Mozilla Public License 2.0](https://img.shields.io/badge/License-Mozilla_Public_License_2.0-brightgreen.svg)
+
+## Description
+This application runs a series of command-line prompts in order to generate a professional looking README.md file for a project. It utilises the Inquirer Node.js module to prompt the user to fill out each section of the README, also selecting a license that their work will be covered under. Once generated, the README will display all answers to the prompts in the appropriate section, and also feature a badge image for the chosen license at the top of the page.
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+Prior to open, install the relevant node modules, particularly Inquirer, type `npm i` into the command line.
+
+## Usage
+Application code is run by typing `node index` into the command line. Follow the prompts, also choosing a license when asked. The generated readme is found in the 'output' folder.
+
+## License
+This application is covered under the Mozilla Public License 2.0 license.
+
+## Contributing
+N/A There are no contribution guidelines for this application.
+
+## Tests
+N/A There is no testing suite for this application.
+
+## Questions
+If you have further questions about this project you can reach me at higgdan@fakeaddress.com  
+
+You can find more of my examples of my work at my Github: https://github.com/higgdan
--- index.js
+++ index.js
@@ -1,41 +1,8 @@
+// define required packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-const generateREADME = ({projectTitle, projectDesc, installation, usage, contribution, testInstr, license, github, email}) =>
-  `# ${projectTitle}
-
-  ## Description
-  ${projectDesc}
-
-  ## Table of Contents
-  
-  - [Installation](#installation)
-  - [Usage](#usage)
-  - [License](#license)
-  - [Contributing](#contributing)
-  - [Tests](#tests)
-  - [Questions](#questions)
-  
-  ## Installation
-  ${installation}
-
-  ## Usage
-  ${usage}
-
-  ## License
-  This applicattion is covered under ${license}
-
-  ## Contributing
-  ${contribution}
-
-  ## Tests
-  ${testInstr}
-
-  ## Questions
-  Github: https://github.com/${github}  
-  Email: ${email}
-  `;
-
+// on application load, prompt the user fill out each section of the README in turn
 inquirer
   .prompt([
     {
@@ -46,7 +13,7 @@ inquirer
     {
       type: 'input',
       name: 'projectDesc',
-      message: 'Briefly describe your project.',
+      message: 'Briefly describe your project:',
     },
     {
       type: 'input',
@@ -56,7 +23,7 @@ inquirer
     {
       type: 'input',
       name: 'usage',
-      message: 'What is the usage information?',
+      message: 'Describe how to use the application',
     },
     {
       type: 'input',
@@ -72,7 +39,7 @@ inquirer
       type: 'list',
       name: 'license',
       message: 'What license covers this application?',
-      choices: ['license1', 'license2', 'license3', 'license4']
+      choices: ['MIT License', 'GNU General Public License v3.0', 'Mozilla Public License 2.0', 'The Unlicense']
     },
     {
       type: 'input',
@@ -86,8 +53,48 @@ inquirer
     },
   ])
   .then((answers) => {
-    console.log(answers);
-    fs.writeFile('./output/README.md', generateREADME(answers), (err) =>
+    // uses a regex to convert spaces to underscores for insertion into badge-making URL
+    let insUnderscore = answers.license.replace(/ /g,"_");
+    // creates a README file, passes in answers to prompts and altered liscence 
+    fs.writeFile('./output/README.md', generateREADME(answers, insUnderscore), (err) =>
       err ? console.log(err) : console.log('Created README.md')
     );
   });
+
+// defines the layout of README and position of the passed in parameters
+const generateREADME = ({projectTitle, projectDesc, installation, usage, contribution, testInstr, license, github, email}, licenseUnd) =>
+`# ${projectTitle}
+
+![License: ${license}](https://img.shields.io/badge/License-${licenseUnd}-brightgreen.svg)
+
+## Description
+${projectDesc}
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+${installation}
+
+## Usage
+${usage}
+
+## License
+This application is covered under the ${license} license.
+
+## Contributing
+${contribution}
+
+## Tests
+${testInstr}
+
+## Questions
+If you have further questions about this project you can reach me at ${email}  
+
+You gan find more of my examples of my work at my Github: https://github.com/${github}`;
